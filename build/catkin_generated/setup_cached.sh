24f6906f7efecc5db27f9c750af8995a36cb5110
bartboogmans <bartboogmans@hotmail.com>
2023-03-22 19:15:40 +0100

    add dependencies on geometry_msgs

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/bart/Documents/Nausbot/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/bart/Documents/Nausbot/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/bart/Documents/Nausbot/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/bart/Documents/Nausbot/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/bart/Documents/Nausbot/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/bart/Documents/Nausbot/src:$ROS_PACKAGE_PATH"
