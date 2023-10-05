2d9dd44a2ee36161aa3011650045c628fdd3082f
Bart <bartboogmans@hotmail.com>
2023-10-05 11:34:25 +0200

    remove wrongly built files

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/bart/Documents/Nausbot/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/bart/Documents/Nausbot/devel;/opt/ros/noetic'.split(';') if '/home/bart/Documents/Nausbot/devel;/opt/ros/noetic' != '' else []
